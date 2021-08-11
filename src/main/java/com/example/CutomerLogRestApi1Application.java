cea2f0bbdd80fe130a5fd185fb31afc1f3516ee5
iamneowise <wise@iamneo.ai>
2021-08-11 20:29:14 +0530

    update the workflow

--- src/main/java/com/example/CutomerLogRestApi1Application.java
+++ src/main/java/com/example/CutomerLogRestApi1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CutomerLogRestApi1Application {
 
 	public static void main(String[] args) {
-		System.out.println("STarted");
+		System.out.println("STarted the application");
 		SpringApplication.run(CutomerLogRestApi1Application.class, args);
 	}
 
